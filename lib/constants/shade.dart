89a952e3ae47794b7b47902e0705d1fe04fed00a
James Lee <jameslee@Jamess-MacBook-Pro.local>
2026-06-24 00:38:28 +0900

    appshade fix;

--- lib/constants/shade.dart
+++ lib/constants/shade.dart
@@ -1,5 +1,7 @@
-// Use elevation to separate background and content
-const double shade_1 = 100;
-const double shade_2 = 200;
-const double shade_3 = 300;
-const double shade_4 = 400;
+// Use elevation to separate background and contentc
+class AppShade {
+  static const double shade_1 = 100;
+  static const double shade_2 = 200;
+  static const double shade_3 = 300;
+  static const double shade_4 = 400;
+}
