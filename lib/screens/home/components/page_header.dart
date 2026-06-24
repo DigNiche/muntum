48cfd7a6f8907da0f57bfc392ff6ab1d7558a5d6
James Lee <jameslee@Jamess-MacBook-Pro.local>
2026-06-24 21:52:16 +0900

    color animation;

---
+++ lib/screens/home/components/page_header.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil_plus/flutter_screenutil_plus.dart';
+import 'package:muntum/constants/colors.dart';
+import 'package:muntum/constants/typography.dart';
+
+// HeaderItem
+class HeaderTabItem extends StatelessWidget {
+  final String text;
+  final Color textColor;
+  final VoidCallback? onTap;
+  const HeaderTabItem({
+    super.key,
+    required this.text,
+    required this.textColor,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Text(text, style: AppTypography.title2.copyWith(color: textColor)),
+    );
+  }
+}
+
+// Header
+class PageHeader extends StatelessWidget {
+  final String firstText;
+  final VoidCallback? onFirstTextTap;
+  final Color firstTextColor;
+  final VoidCallback? onSecondTextTap;
+  final String? secondText;
+  final Color? secondTextColor;
+  final Widget icon;
+  final bool showIndicator;
+  const PageHeader({
+    super.key,
+    required this.firstText,
+    this.onFirstTextTap,
+    this.onSecondTextTap,
+    this.secondText,
+    required this.icon,
+    required this.firstTextColor,
+    this.secondTextColor,
+    required this.showIndicator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 20.h, horizontal: 20.h),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              if (showIndicator)
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    HeaderTabItem(
+                      text: firstText,
+                      textColor: AppColors.gray300,
+                      onTap: onFirstTextTap,
+                    ),
+                    Positioned(
+                      right: -5.w,
+                      child: Container(
+                        width: 4.w,
+                        height: 4.h,
+                        decoration: BoxDecoration(
+                          color: AppColors.primary400,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              if (!showIndicator)
+                HeaderTabItem(
+                  text: firstText,
+                  textColor: AppColors.gray300,
+                  onTap: onFirstTextTap,
+                ),
+              if (secondText != null) SizedBox(width: 20.w),
+              if (secondText != null)
+                HeaderTabItem(
+                  text: secondText!,
+                  textColor: secondTextColor!,
+                  onTap: onSecondTextTap,
+                ),
+            ],
+          ),
+          icon,
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home/my_niche_screen.dart
+++ lib/screens/home/my_niche_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:muntum/screens/home/components/pageHeader.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil_plus/flutter_screenutil_plus.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:muntum/constants/colors.dart';
+import 'package:muntum/screens/home/components/page_header.dart';
+
+enum ScreenTypes { myNiche, entire }
 
 class MyNicheScreen extends StatefulWidget {
   const MyNicheScreen({super.key});
@@ -9,18 +15,66 @@ class MyNicheScreen extends StatefulWidget {
 }
 
 class _MyNicheScreenState extends State<MyNicheScreen> {
+  ScreenTypes screenType = ScreenTypes.myNiche;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              HeaderTabItem(text: '내취향', textColor: TabTextColor.black),
-            ],
-          ),
-        ],
+    final isMyNiche = screenType == ScreenTypes.myNiche;
+
+    return TweenAnimationBuilder<Color?>(
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+      tween: ColorTween(
+        end: isMyNiche ? AppColors.gray900 : AppColors.backgroundNormal,
       ),
+      builder: (context, animatedBackgroundColor, child) {
+        return AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            // Android
+            statusBarIconBrightness: isMyNiche
+                ? Brightness.light
+                : Brightness.dark,
+            // iOS
+            statusBarBrightness: isMyNiche ? Brightness.dark : Brightness.light,
+          ),
+          child: Scaffold(
+            backgroundColor: animatedBackgroundColor,
+            body: Column(
+              children: [
+                SizedBox(height: 50.h),
+                PageHeader(
+                  firstText: '내취향',
+                  firstTextColor: isMyNiche
+                      ? AppColors.white
+                      : AppColors.gray300,
+                  onFirstTextTap: () {
+                    setState(() {
+                      screenType = ScreenTypes.myNiche;
+                    });
+                  },
+                  secondText: '전체',
+                  secondTextColor: isMyNiche
+                      ? AppColors.gray600
+                      : AppColors.black,
+                  onSecondTextTap: () {
+                    setState(() {
+                      screenType = ScreenTypes.entire;
+                    });
+                  },
+                  icon: SvgPicture.asset(
+                    'assets/icons/search.svg',
+                    width: 24.sp,
+                    height: 24.sp,
+                    color: isMyNiche ? AppColors.white : AppColors.gray600,
+                  ),
+                  showIndicator: isMyNiche,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
